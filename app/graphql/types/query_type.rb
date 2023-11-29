4de368e83bd0cd896740b1cedb02b84d92ba4f49
FadyGerges23 <fady.gerges@espace.com.eg>
2023-11-29 18:13:49 +0200

    Added search parameters for posts and user_posts queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -59,17 +59,34 @@ module Types
       end
     end
 
+    
+    def generate_tags_query(tags_ids)
+      query = []
+      if tags_ids && tags_ids.length > 0
+        tags_ids.each do |tag_id|
+          query << "q[id_in][]=#{tag_id}"
+        end
+        tags_query = query.join("&")
+      end
+    end
+
+
     field :user_posts, Types::PaginatedPostType do
       argument :userId, ID
       argument :pageNumber, String, required: false, default_value: "1"
+      argument :title, String, required: false, default_value: ""
+      argument :description, String, required: false, default_value: ""
+      argument :category_id, ID, required: false, default_value: ""
+      argument :tags_ids, [ID], required: false, default_value: []
     end
 
-    def user_posts(userId:, pageNumber:)
+    def user_posts(userId:, pageNumber:, title:, description:, category_id:, tags_ids:)
       bearer_token = context[:headers]['Authorization']&.split('Bearer ')&.last
+      tags_query = generate_tags_query(tags_ids)
     
-      uri = URI("http://localhost:3000/users/#{userId}/posts/page/#{pageNumber}")
+      uri = URI("http://localhost:3000/users/#{userId}/posts/page/#{pageNumber}?q[title_cont]=#{title}&q[body_cont]=#{description}&q[category_id_eq]=#{category_id}&#{tags_query}")
       http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Get.new(uri.path, { 
+      request = Net::HTTP::Get.new(uri, { 
         'Accept' => 'application/json',
         'Authorization' => "Bearer #{bearer_token}"
       })
@@ -113,6 +130,7 @@ module Types
       end
     end
 
+    
     field :categories, [Types::CategoryType]
 
     def categories
@@ -156,12 +174,18 @@ module Types
     
     field :posts, PaginatedPostType do
       argument :pageNumber, String, required: false, default_value: "1"
+      argument :title, String, required: false, default_value: ""
+      argument :description, String, required: false, default_value: ""
+      argument :category_id, ID, required: false, default_value: ""
+      argument :tags_ids, [ID], required: false, default_value: []
     end
 
-    def posts(pageNumber:)
-      uri = URI("http://localhost:3000/posts/page/#{pageNumber}")
+    def posts(pageNumber:, title:, description:, category_id:, tags_ids:)
+      tags_query = generate_tags_query(tags_ids)
+      
+      uri = URI("http://localhost:3000/posts/page/#{pageNumber}?q[title_cont]=#{title}&q[body_cont]=#{description}&q[category_id_eq]=#{category_id}&#{tags_query}")
       http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Get.new(uri.path, { 'Accept' => 'application/json' })
+      request = Net::HTTP::Get.new(uri, { 'Accept' => 'application/json' })
 
       response = http.request(request)
       data = JSON.parse(response.body)
